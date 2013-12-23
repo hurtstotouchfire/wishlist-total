674b40a7c8ced17feaa162eef711d27c9b4f4aa6
ja-barr <crimson@crimsonfae.net>
2013-12-23 00:04:01 -0800

    added sessionstorage to save tab id associate with wishlist id

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 console.log("background running");
 var total = 0.0;
-wishlist_request = {};
-
+if (!wishlist_request) {
+  var wishlist_request = {};
+}
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     chrome.pageAction.show(sender.tab.id);
     sendResponse({"success": "ok", "request":request});
     request.tab_id = sender.tab.id;
     wishlist_request = request;
+    sessionStorage.setItem(sender.tab.id, request.wishlist_id);
   });
