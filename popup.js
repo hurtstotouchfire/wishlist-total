9be59d2884e51752ac423d922cc605091f48fb6b
ja-barr <crimson@crimsonfae.net>
2013-12-22 19:22:10 -0800

    get wishlist id to be set to the correct element (a sub instead of a span)d

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 bg = chrome.extension.getBackgroundPage()
 $(document).ready(function() {
   $("span.price").text(Number(bg.document.total).toFixed(2));
-  $("span.wishlist_id").text(bg.document.wishlist_id);
+  $("sub.wishlist_id").text(bg.document.wishlist_id);
 });
